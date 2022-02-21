cafec0fdb50b17d506f1762ece89305ebbe9389a
Sepe19 <seba217i@stud.kea.dk>
2022-02-21 14:06:44 +0100

    add -gitignore

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const app = express();
+const app = express();    
 
-app.get("/", function(req, res) {
-    res.send("working");
-})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/clock.html");
+});
 
 app.listen(process.env.PORT || 5000);
